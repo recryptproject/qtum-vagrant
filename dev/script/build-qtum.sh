b3159eaa2933eee61f6414ea875066ae20ccc328
Baiqiang <qiyuuu@gmail.com>
2017-10-31 13:57:26 +0800

    Add dev dir

---
+++ dev/script/build-qtum.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+dir=`dirname $0`
+#add aliyun source
+aliyunSources=`cat << AliyunSources
+deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse
+AliyunSources`
+sources=`echo "$aliyunSources" | cat - /etc/apt/sources.list`
+echo "$sources" > /etc/apt/sources.list
+apt-get install -y software-properties-common
+add-apt-repository ppa:bitcoin/bitcoin
+apt-get update
+apt-get install -y libdb4.8-dev libdb4.8++-dev
+apt-get install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev
+
+git clone https://github.com/qtumproject/qtum --recursive
+cd qtum
+./autogen.sh
+./configure && make && make install
